f3a8560f6b22b92faa48eb5a2971c54585adec85
Arun Mukesh <arunmukesh0311gmail.com>
2026-09-05 17:07:25 +0530

    setup spring boot backend foundation

---
+++ backend/src/main/java/com/ztplatform/ZtPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.ztplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtPlatformApplication.class, args);
+    }
+}
